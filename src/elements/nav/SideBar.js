94ff6bc4ee00ec9bc883cce07bb3cfa7dd02bfc6
AuroreLeblois <leblois-aurore@hotmail.fr>
2021-06-17 20:47:10 +0200

    sideNav base

--- src/elements/nav/SideBar.js
+++ src/elements/nav/SideBar.js
@@ -21,9 +21,7 @@ class SideBar extends React.Component {
 	render() {
 		if (!this.state.options.length) return null
 		return (
-		<div style={{
-			textColor: this.props.textColor ? this.props.textColor : 'inherit',
-			backgroundColor: this.props.backgroundColor ? this.props.backgroundColor : 'lightgrey'}} key='sidenav-wrapper-kariu' className={'sidenav-kariu'}>
+		<div style={{ backgroundColor: this.props.backgroundColor ? this.props.backgroundColor : 'lightgrey'}} key='sidenav-wrapper-kariu' className={'sidenav-kariu'}>
 			{this.renderOptions()}
 		</div>
 		 )
@@ -33,7 +31,8 @@ class SideBar extends React.Component {
 		let options = []
 
 		for (let option of this.state.options) {
-			let link = <a className='link' key={option.label} href={option.href}>{option.label}</a>
+			let link = <a className='link' style={{
+				textColor: this.props.textColor ? this.props.textColor : 'inherit'}} key={option.label} href={option.href}>{option.label ? option.label : option.icon}</a>
 			options.push(link)
 		}
 		return[ options ]
