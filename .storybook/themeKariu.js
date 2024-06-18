995019aee90fabf9f064cee9af7899fff4f9b152
AuroreLeblois <leblois-aurore@hotmail.fr>
2024-06-18 20:53:16 +0200

    minor changes

--- .storybook/themeKariu.js
+++ .storybook/themeKariu.js
@@ -1,36 +1,37 @@
-import { create } from '@storybook/theming'
-import logo from './favicon.svg'
+import { create } from "@storybook/theming";
+import logo from "./favicon.svg";
 
 export default create({
-	base: 'light',
-	title: 'React-kariu',
-	brandTitle: 'React-Kariu',
-	brandImage: logo,
+  base: "light",
+  title: "React-kariu",
+  brandTitle: "react-kariu",
+  brandImage: logo,
+  brandTarget: "_self",
 
-	colorPrimary: '#3C1B43',
- 	colorSecondary: '#501537',
-	// UI
-	appBg: 'white',
-	appContentBg: 'white',
-	appBorderColor: 'white',
-	appBorderRadius: 4,
+  colorPrimary: "#3C1B43",
+  colorSecondary: "#501537",
+  // UI
+  appBg: "white",
+  appContentBg: "white",
+  appBorderColor: "white",
+  appBorderRadius: 4,
 
-	// Text colors
-	textColor: 'black',
-	textInverseColor:  '#3C1B43',
+  // Text colors
+  textColor: "black",
+  textInverseColor: "#3C1B43",
 
-	// Toolbar default and active colors
-	barTextColor: '#3C1B43',
-	barSelectedColor: 'black',
-	barBg: '#922D50',
+  // Toolbar default and active colors
+  barTextColor: "#3C1B43",
+  barSelectedColor: "black",
+  barBg: "#922D50",
 
-	// Form colors
-	inputBg: '#922D50',
-	inputBorder: '#3C1B43',
-	inputTextColor: '#3C1B43',
-	inputBorderRadius: 4,
+  // Form colors
+  inputBg: "#922D50",
+  inputBorder: "#3C1B43",
+  inputTextColor: "#3C1B43",
+  inputBorderRadius: 4,
 
-	// Typography
-	fontBase: '"Open Sans", sans-serif',
-	fontCode: 'monospace',
-})
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+});
