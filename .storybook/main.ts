63473f3d0af3e6af52848c4550fcce91f7ab78df
AuroreLeblois <leblois-aurore@hotmail.fr>
2024-06-22 21:56:15 +0200

    basic documentation added

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,17 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../stories/**/*.mdx", 
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    '@storybook/addon-docs'
   ],
   framework: {
     name: "@storybook/react-vite",
